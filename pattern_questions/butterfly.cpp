c5711a3c7dc74477fee218a9878887daa1fe266a
KaustubhBankar <kaustubhbankar12@gmail.com>
2025-08-16 12:07:06 +0530

    Add butterfly pattern generation implementation in C++

---
+++ pattern_questions/butterfly.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+   int n = 4;
+
+   for(int i=1; i<=n; i++){
+    for(int j=1; j<=i; j++){
+        cout<< "*";
+    }
+    for(int sp=1; sp<=2*(n-i); sp++){
+        cout<< " ";
+    }
+    for(int j=1; j<=i; j++){
+        cout<< "*";
+    }
+    cout<< endl;
+   }
+
+   for(int i=n; i>=1; i--){
+    for(int j=1; j<=i; j++){
+        cout<< "*";
+    }
+    for(int sp=1; sp<=2*(n-i); sp++){
+        cout<< " ";
+    }
+    for(int j=1; j<=i; j++){
+        cout<< "*";
+    }
+    cout<< endl;
+   }
+
+   
+   return 0;
+}
