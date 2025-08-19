bd15a9874c7fb18703f5b21bfa493ad0d9419808
KaustubhBankar <kaustubhbankar12@gmail.com>
2025-08-19 12:39:37 +0530

    Add optimal subarray sum implementation in C++

---
+++ subArraySum.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+// Optimal approach TC=O(n), SC=O(n);
+int subarraySum(vector<int> arr, int k)
+{
+   int n = arr.size();
+   int count = 0;
+   vector<int> prefixSum(n, 0);
+
+   prefixSum[0] = arr[0];
+   for (int i = 1; i < n; i++)
+   {
+      prefixSum[i] = prefixSum[i - 1] + arr[i];
+   }
+
+   unordered_map<int, int> m;
+   for (int j = 0; j < n; j++)
+   {
+      if (prefixSum[j] == k)
+      {
+         count++;
+      }
+      int val = prefixSum[j] - k;
+
+      if (m.find(val) != m.end())
+      {
+         count += m[val];
+      }
+      if (m.find(prefixSum[j]) == m.end())
+      {
+         m[prefixSum[j]] = 0;
+      }
+      m[prefixSum[j]]++;
+   }
+   return count;
+}
+
+// using brute force approach O(n^2)
+// int subarraySum(vector<int> arr, int k){
+//     int n = arr.size();
+//     int count = 0;
+//     for(int i=0; i<n; i++){
+//         int sum = 0;
+//         for(int j=i; j<n; j++){
+//             sum += arr[j];
+//             if(sum == k){
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// }
+
+int main()
+{
+   vector<int> arr = {1, 1, 1};
+   int k = 2;
+
+   cout << subarraySum(arr, k);
+   return 0;
+}
